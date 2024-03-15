ae779711a223233e50e92430320fbd9858f4926b
Yousef <yousef_758@outlook.com>
2024-03-15 16:29:38 +0200

    most deletion tests done; only exceptions test and multiple deletion test remain

--- src/test/java/com/goat/database/DeletionTests.java
+++ src/test/java/com/goat/database/DeletionTests.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -15,8 +16,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.Hashtable;
+import java.util.Properties;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 @SuppressWarnings("removal")
 public class DeletionTests {
@@ -28,6 +31,7 @@ public class DeletionTests {
 	Tuple tupleForComparing;
 	Page page1;
 	Page page2;
+	Page page3;
 
 	@BeforeEach
 	void init() throws IOException, ClassNotFoundException, DBAppException
@@ -40,13 +44,20 @@ public class DeletionTests {
                  try {Files.delete(path);} 
                  catch (IOException e) {e.printStackTrace();}});
         
-		new File("./resources/tables.ser").delete();
 		new File("./resources/metadata.csv").delete();
 
+		File configFile = new File("resources/DBApp.config");
+		Properties props = new Properties();
+	    props.setProperty("MaximumRowsCountinPage", "3");
+	    FileWriter writer = new FileWriter(configFile);
+	    props.store(writer, "");
+	    writer.close();
+	    
 		database = new DBApp();
 
-		page1 = new Page();
-		page2 = new Page();
+		page1 = new Page("table0.ser",0,"tables/table/");
+		page2 = new Page("table1.ser",1,"tables/table/");
+		page3 = new Page("table2.ser",2,"tables/table/");
 
 		htbl = new Hashtable<String,String>();
 		htbl.put("id", "java.lang.Integer");
@@ -54,6 +65,7 @@ public class DeletionTests {
 
 		colData = new Hashtable<String, Object>();
 		table = database.tables.get(0);
+		
 	}
 	public void serializedata(Object o, String filename) throws IOException 
 	{
@@ -73,6 +85,7 @@ public class DeletionTests {
 		return output;
 	}
 	
+	//-------------------------------------------------------TESTS--------------------------------------------------------------
 	//This checks if one tuple is deleted from the page
 	@Test
 	void Delete_Tuple_From_Page() throws IOException, DBAppException, ClassNotFoundException
@@ -80,21 +93,23 @@ public class DeletionTests {
 		colData.put("id", new Integer(5));
 		Tuple tuple = new Tuple(5,colData);
 
-		colData.clear();
+		colData = new Hashtable<String, Object>();
 		colData.put("id", new Integer(10));
 		Tuple tobedeleted = new Tuple(6,colData);
 
-		page1.name = "table1.ser";
 		page1.tuples.add(tuple);
 		page1.tuples.add(tobedeleted);
 		table.pageNames.add(page1.name);
+		table.numberForPage = 1;
 		
-		serializedata(page1,table.filepath + page1.name);
+		page1.serializePage();
 		database.deleteFromTable("table", colData);
 		
-		page1 = (Page) deserializeData("./tables/table/"+page1.name);
-		assertTrue(page1.tuples.size()==1 && page1.tuples.get(0).equals(tuple),
-				"Tuple was not deleted");
+		page1 = (Page) deserializeData(page1.pageFilepath);
+		System.out.println(page1.tuples.toString());
+		assertTrue(page1.tuples.size()==1, "Expected one tuple to be in the page, but instead there are "
+				+ page1.tuples.size() + " tuples in the page");
+		assertTrue(page1.tuples.get(0).compareTo(tuple)==0, "Expected tuple " + tuple + ", but instead got tuple " + page1.tuples.get(0));
 	}
 
 	// This checks that if multiple tuples satisfy the deletion condition,
@@ -109,28 +124,38 @@ public class DeletionTests {
 	@Test
 	void No_Empty_Space_In_Page_After_Deletion() throws DBAppException, ClassNotFoundException, IOException
 	{
+		colData.put("id", new Integer(2));
+		Tuple tuple1 = new Tuple(2,colData);
+		
+		colData = new Hashtable<String, Object>();
 		colData.put("id", new Integer(5));
 		Tuple tobedeleted = new Tuple(5,colData);
 
-		colData.clear();
+		colData = new Hashtable<String, Object>();
 		colData.put("id", new Integer(10));
-		Tuple tuple = new Tuple(6,colData);
+		Tuple tuple2 = new Tuple(6,colData);
 
-		page1.name = "table1.ser";
+		page1.tuples.add(tuple1);
 		page1.tuples.add(tobedeleted);
-		page1.tuples.add(tuple);
+		page1.tuples.add(tuple2);
 		table.pageNames.add(page1.name);
+		table.numberForPage = 1;
 		
-		serializedata(page1, table.filepath +page1.name);
+		page1.serializePage();
 		
-		colData.clear();
+		colData = new Hashtable<String, Object>();
 		colData.put("id", new Integer(5));
 		database.deleteFromTable("table", colData);
 		
-		page1 = (Page) deserializeData(table.filepath+page1.name);
+		page1 = (Page) deserializeData(page1.pageFilepath);
 		
-		assertTrue(page1.tuples.size()==1 && page1.tuples.get(0).equals(tuple),
-				"Tuple was not either deleted or shifted");
+		assertTrue(page1.tuples.size()==2, "Expected number of tuples in page to be two, but instead there are "
+				+ "" + page1.tuples.size() + " tuples in the page");
+		assertTrue(page1.tuples.get(0).compareTo(tuple1)==0, "Expected first tuple in page to be " + tuple1 + " but instead got "
+				   + page1.tuples.get(0));		
+		assertTrue(page1.tuples.get(1).compareTo(tuple2)==0, "Expected second tuple in page to be " + tuple2 + " but instead got "
+				+ page1.tuples.get(1));
+		
 	}
 
 	// This checks that if we delete the last tuple from a page, it
@@ -141,24 +166,24 @@ public class DeletionTests {
 		colData.put("id", new Integer(5));
 		Tuple tuple = new Tuple(5,colData);
 
-		page1.name = "table1.ser";
 		page1.tuples.add(tuple);
 		table.pageNames.add(page1.name);
+		table.numberForPage = 1;
 		
-		serializedata(page1, table.filepath+page1.name);
+		page1.serializePage();
 
 		database.deleteFromTable("table", colData);
 		
-		assertTrue(table.pageNames.size()==0, "Page name was not deleted "
-				+ "from the object table");
+		assertTrue(table.pageNames.size()==0, "Page name was not deleted from the table");
 		
 		assertThrows(FileNotFoundException.class, () ->
-		{deserializeData(table.filepath+page1.name);});
+		{deserializeData(page1.pageFilepath);}, "Page was not deleted from the hard disk");
 	}
 
 	// This checks that deleting the tuple also deletes it from the
 	// index (if it exists)
-//	@Test 
+	@Test 
+	@Disabled
 	void Delete_Tuple_From_Index_Too()
 	{
 
